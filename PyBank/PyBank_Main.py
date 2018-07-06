e9b5ab23ad06d04252980d21648c6946ace1b85f
Charla Myers <38386348+charlasu@users.noreply.github.com>
2018-07-06 12:07:15 -0700

    v2 ...

--- PyBank/PyBank_Main.py
+++ PyBank/PyBank_Main.py
@@ -18,9 +18,13 @@ budget_data_path = os.path.join("Resources", "budget_data.csv")
 #Create variables: DO NOT TOUCH VARIABLES
 total_months = -1
 total_net = 0
-average_change = {}
-monthly_averages = [row[1]]
+avg_change = []
+avg_change_list = []
+#monthly_avg = [row[1]]
+previous_net = int
 month = 1
+best_increase = ["", 0]
+worst_decrease = ["", 99999999999999999999999999999999999999]
 
 #open and read csv and set the comma as the separator: DO NOT TOUCH WITH STATEMENT 
 with open(budget_data_path, newline="") as budget_data_raw:
@@ -31,23 +35,57 @@ with open(budget_data_path, newline="") as budget_data_raw:
 	for row in reader:
 		total_months = total_months + 1
 
-	total_net += int(row[1])
+		if int(column[2],row[2]) >= int(best_increase):
+			best_increase = int(column[2],row[2])
+			best_increase_month = row[0]
+
+		if int(row[1]) <= int(worst_decrease):
+			worst_decrease = int(row[1])
+			worst_decrease_month = row[0]
+		total_net += int(row[1])
+
+		#avg_change = int(row[1]) - int(previous_net)
+		#avg_change_list = avg_change_list + [avg_change]
+		#monthly_avg = monthly_avg + [row[0]]
+		#net_month_avg = sum(avg_change_list)/len(avg_change_list)
+
+#calculate average_change
+average_change = round(total_profit_loss/total_months, 2)
+
+#Best monthly increase
+#if avg_change > best_increase[1]:
+	#best_increase[0] = row[0]
+	#best_increase[1] = avg_change
+
+#Worst monthly increase
+#if avg_change > worst_increase[1]:
+	#worst_increase[0] = row[0]
+	#worst_increase[1] = avg_change
 
 
-def moving_average(values, size):
-	for _ in range(size 2)
-		yield None
-	for selection window(values, size):
-		yield sum(selection)/size
-	
 
 
 #Print summary: DO NOT TOUCH PRINT SUMMARY
 print("Financial Analysis")
 print("-------------------------")
 print("Total Months: " + str(total_months))
-print("Total Net: " + str(total_net))
+print("Total Net: " + str(total_revenue))
 #print("Average Monthly Change: " + str(average_change(average_change, 2)))
 #print("Best Monthly Profit Increase: " + str(increase))
 #print("Worst Monthly Profit Decrease: " + str(decrease))
+
+
+
+
+
+
+
+
+#Create the output of the text file: DO NOT TOUCH
+output_results = open("output_results.txt", "w")
+output_results.writelines(["Financial Analysis\n", "------------------------\n","Total Months: " + str(total_months), "\n", str(total_net), "\n"])
+output_results.close()
+checkfile("output_results.txt")
+
+
 
