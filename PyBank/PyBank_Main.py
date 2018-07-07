91ce91dd8910031b3c9e2d93c76b3d9b07d00218
Charla Myers <38386348+charlasu@users.noreply.github.com>
2018-07-06 18:44:24 -0700

    Add files via upload

--- PyBank/PyBank_Main.py
+++ PyBank/PyBank_Main.py
@@ -12,67 +12,57 @@
 import os
 import csv
 
-#csv path: DO NOT TOUCH PATH
+# The directory path for csv file with election data: 
 budget_data_path = os.path.join("Resources", "budget_data.csv")
 
-#Create variables: DO NOT TOUCH VARIABLES
+#Set variable for total months 
 total_months = -1
-total_net = 0
-avg_change = []
-avg_change_list = []
-#monthly_avg = [row[1]]
-previous_net = int
-month = 1
-best_increase = ["", 0]
-worst_decrease = ["", 99999999999999999999999999999999999999]
 
-#open and read csv and set the comma as the separator: DO NOT TOUCH WITH STATEMENT 
+#Open and read csv and set the comma as the separator 
 with open(budget_data_path, newline="") as budget_data_raw:
 	reader = csv.reader(budget_data_raw, delimiter=",")
 
+#Skips reading the header row
 	csv_header = next(reader)
 
-	for row in reader:
-		total_months = total_months + 1
 
-		if int(column[2],row[2]) >= int(best_increase):
-			best_increase = int(column[2],row[2])
-			best_increase_month = row[0]
+#Set variables, lists, etc.
+#Create lists for best and worst ... something must be worse than all those 9s
+best_increase = ["", 0]
+worst_decrease = ["", 99999999999999999999999999999999999999]
 
-		if int(row[1]) <= int(worst_decrease):
-			worst_decrease = int(row[1])
-			worst_decrease_month = row[0]
-		total_net += int(row[1])
+#This"for loop" finds total months 
+for row in reader:
+	total_months = total_months + 1
+	print(total_months)
 
-		#avg_change = int(row[1]) - int(previous_net)
-		#avg_change_list = avg_change_list + [avg_change]
-		#monthly_avg = monthly_avg + [row[0]]
-		#net_month_avg = sum(avg_change_list)/len(avg_change_list)
 
-#calculate average_change
-average_change = round(total_profit_loss/total_months, 2)
+#This"while loop" tracks variance in the P&L	
+net = 0
+total_profit_loss = [row[1]]
+
+while total_profit_loss > net:
+	previous_net = total_profit_loss - (net(row[1])
+
+#Calculate average_change
+	average_change = []
+	average_change = round(total_profit_loss/total_months, 2)
 
 #Best monthly increase
 #if avg_change > best_increase[1]:
 	#best_increase[0] = row[0]
-	#best_increase[1] = avg_change
+	#best_increase[1] = average_change
 
 #Worst monthly increase
 #if avg_change > worst_increase[1]:
 	#worst_increase[0] = row[0]
-	#worst_increase[1] = avg_change
-
-
+	#worst_increase[1] = average_change
 
 
 #Print summary: DO NOT TOUCH PRINT SUMMARY
-print("Financial Analysis")
-print("-------------------------")
-print("Total Months: " + str(total_months))
-print("Total Net: " + str(total_revenue))
-#print("Average Monthly Change: " + str(average_change(average_change, 2)))
-#print("Best Monthly Profit Increase: " + str(increase))
-#print("Worst Monthly Profit Decrease: " + str(decrease))
+print("Financial Analysis \n ------------------------- \n Total Months: " + str(total_months) + "Total Net: " + str(total_profit_loss))
+
+#p + "Average Monthly Change: " + str(average_change(average_change) + "Best Monthly Profit Increase: " + str(increase) + "Worst Monthly Profit Decrease: " + str(decrease))
 
 
 
